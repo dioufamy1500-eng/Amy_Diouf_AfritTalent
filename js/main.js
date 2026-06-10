f9a37f5771e1e2f23a7784ca17b1f40689a8a722
dioufamy1500-eng <dioufamy@1500gmail.com>
2026-06-10 21:39:44 +0000

    JS : compteurs animés au scroll et animations fade-in des sections

--- js/main.js
+++ js/main.js
@@ -50,10 +50,55 @@ window.addEventListener("scroll", () => {
 backToTop.addEventListener("click", () =>{
     window.scrollTo({
         top:0,
-        // behavior:"smooth"
     })
 })
 
+// compteur 
+
+const counters = document.querySelectorAll(".counter");
+function animate(counter){
+  const target = Number(counter.dataset.bsTarget);
+  let current = 0;
+
+  const timer = setInterval(() => {
+    current = current + 10;
+
+    counter.textContent = "+" + current;
+    if (current >= target){
+      counter.textContent = "+" + target;
+      clearInterval(timer);
+    }
+  },30)
+}
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      animate(entry.target);
+    }
+  });
+});
+
+counters.forEach((counter) => {
+  counter.textContent = "+0";
+  observer.observe(counter);
+});
+
+// fad-Animation
+
+const sections = document.querySelectorAll(".fad-section");
+
+const regarder = new IntersectionObserver((elementsVisibles) => {
+  elementsVisibles.forEach((elementVisible) => {
+    if (elementVisible.isIntersecting) {
+      elementVisible.target.classList.add("show");
+    }
+  });
+});
+
+sections.forEach((section) => {
+ regarder.regarder(section);
+});
 
 
 
